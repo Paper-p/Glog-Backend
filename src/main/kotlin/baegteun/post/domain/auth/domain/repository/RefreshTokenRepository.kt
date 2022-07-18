214cb3222240f63faa929a4102271086a38c9483
baegteun <baegteun@gmail.com>
2022-07-19 08:12:20 +0900

    :sparkles: :: RefreshTokenRepository

---
+++ src/main/kotlin/baegteun/post/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -0,0 +1,7 @@
+package baegteun.post.domain.auth.domain.repository
+
+import baegteun.post.domain.auth.domain.entity.RefreshToken
+import org.springframework.data.repository.CrudRepository
+
+interface RefreshTokenRepository: CrudRepository<RefreshToken, Long> {
+}
