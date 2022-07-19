ab11d23bfde7e416aefbeec88c4f83a8d72f3338
baegteun <baegteun@gmail.com>
2022-07-19 10:40:29 +0900

    :sparkles: :: JwtTokenFilter

---
+++ src/main/kotlin/baegteun/post/global/security/jwt/JwtTokenFilter.kt
@@ -0,0 +1,24 @@
+package baegteun.post.global.security.jwt
+
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtTokenFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+): OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = jwtTokenProvider.resolveToken(request)
+        if (token != null) {
+            val auth = jwtTokenProvider.authentication(token)
+            SecurityContextHolder.getContext().authentication = auth
+        }
+        filterChain.doFilter(request, response)
+    }
+}
