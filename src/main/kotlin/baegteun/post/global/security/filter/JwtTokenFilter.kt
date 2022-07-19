080fbacf346ca5ce79ecfbb680a1fbff8ba08b1d
baegteun <baegteun@gmail.com>
2022-07-19 10:40:52 +0900

    :truck: :: JwtTokenFilter / jwt -> filter

--- src/main/kotlin/baegteun/post/global/security/filter/JwtTokenFilter.kt
+++ src/main/kotlin/baegteun/post/global/security/filter/JwtTokenFilter.kt
@@ -1,5 +1,6 @@
-package baegteun.post.global.security.jwt
+package baegteun.post.global.security.filter
 
+import baegteun.post.global.security.jwt.JwtTokenProvider
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
