1c87aa839ee9c251407ff31fcf329e1531c6a954
baegteun <baegteun@gmail.com>
2022-07-19 09:25:22 +0900

    :sparkles: :: AuthDetails

--- src/main/kotlin/baegteun/post/global/security/auth/AuthDetails.kt
+++ src/main/kotlin/baegteun/post/global/security/auth/AuthDetails.kt
@@ -1,4 +1,24 @@
 package baegteun.post.global.security.auth
 
-class AuthDetails {
+import baegteun.post.domain.user.domain.entity.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val user: User
+): UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority>? = null
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = user.userId
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+
 }
--- src/main/kotlin/baegteun/post/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/baegteun/post/global/security/auth/AuthDetailsService.kt
@@ -1,4 +1,16 @@
 package baegteun.post.global.security.auth
 
-class AuthDetailsService {
+import baegteun.post.domain.user.domain.repository.UserRepository
+import baegteun.post.domain.user.exception.UserNotFoundException
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+
+class AuthDetailsService(
+    private val userRepository: UserRepository
+): UserDetailsService {
+    override fun loadUserByUsername(username: String?): UserDetails =
+        userRepository.findByUserId(username ?: "")?.let {
+            AuthDetails(it)
+        } ?: throw UserNotFoundException.EXCEPTION
+
 }
