44aa8876a0d3937e3179a0e176a735204d461c48
baegteun <baegteun@gmail.com>
2022-07-22 12:06:52 +0900

    :sparkles: :: FetchUserProfile service

---
+++ src/main/kotlin/baegteun/post/domain/user/services/FetchUserProfileService.kt
@@ -0,0 +1,23 @@
+package baegteun.post.domain.user.services
+
+import baegteun.post.domain.user.presentation.dto.response.MyPageResponseDto
+import baegteun.post.domain.user.presentation.dto.response.UserProfileResponseDto
+import baegteun.post.domain.user.utils.UserUtil
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = true)
+@Service
+class FetchUserProfileService(
+    private val userUtil: UserUtil
+) {
+    fun execute(userId: String): ResponseEntity<UserProfileResponseDto> {
+        val user = userUtil.fetchUserByUserId(userId)
+        val response = MyPageResponseDto(
+            user,
+            user.feeds
+        )
+        return ResponseEntity.ok().build()
+    }
+}
