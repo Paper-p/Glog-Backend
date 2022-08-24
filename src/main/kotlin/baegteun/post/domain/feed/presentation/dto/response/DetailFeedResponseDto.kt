569173722b04fc88a2981a33224128df23079333
baegteun <baegteun@gmail.com>
2022-08-24 11:50:23 +0900

    update responseDto

--- src/main/kotlin/baegteun/post/domain/feed/presentation/dto/response/DetailFeedResponseDto.kt
+++ src/main/kotlin/baegteun/post/domain/feed/presentation/dto/response/DetailFeedResponseDto.kt
@@ -14,16 +14,30 @@ data class DetailFeedResponseDto(
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     val createdAt: ZonedDateTime,
     val hit: Int,
+    val likeCount: Int,
+    val isLike: Boolean,
+    val tagList: List<String>,
     val author: AuthorDto,
     val isMine: Boolean
 ) {
-    constructor(feed: Feed, imageUrls: List<String>, hitCount: Int, isMine: Boolean): this(
+    constructor(
+        feed: Feed,
+        imageUrls: List<String>,
+        hitCount: Int,
+        likeCount: Int,
+        isLike: Boolean,
+        tagList: List<String>,
+        isMine: Boolean
+    ): this(
         feed.id,
         feed.title,
         feed.content,
         imageUrls,
         feed.createdAt.atZone(ZoneId.of("Asia/Seoul")),
         hitCount,
+        likeCount,
+        isLike,
+        tagList,
         AuthorDto(feed.user),
         isMine
     )
