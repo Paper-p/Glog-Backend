634f1156a521e182e521a76d23d35f98a7eec573
baegteun <baegteun@gmail.com>
2022-08-24 11:49:38 +0900

    new property 대응

--- src/main/kotlin/baegteun/post/domain/feed/services/DetailFeedService.kt
+++ src/main/kotlin/baegteun/post/domain/feed/services/DetailFeedService.kt
@@ -1,13 +1,11 @@
 package baegteun.post.domain.feed.services
 
-import baegteun.post.domain.feed.domain.entity.FeedImage
-import baegteun.post.domain.feed.domain.repository.FeedImageRepository
-import baegteun.post.domain.feed.domain.repository.FeedRepository
-import baegteun.post.domain.feed.exception.FeedNotFoundException
 import baegteun.post.domain.feed.presentation.dto.response.DetailFeedResponseDto
 import baegteun.post.domain.feed.utils.FeedUtil
+import baegteun.post.domain.heart.domain.repository.HeartRepository
 import baegteun.post.domain.hit.domain.entity.Hit
 import baegteun.post.domain.hit.domain.repository.HitRepository
+import baegteun.post.domain.tag.domain.repository.TagRepository
 import baegteun.post.domain.user.utils.UserUtil
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -19,6 +17,8 @@ import java.util.Objects
 @Service
 class DetailFeedService(
     private val hitRepository: HitRepository,
+    private val heartRepository: HeartRepository,
+    private val tagRepository: TagRepository,
     private val userUtil: UserUtil,
     private val feedUtil: FeedUtil
 ) {
@@ -28,8 +28,21 @@ class DetailFeedService(
         val hit = hitRepository.findById(feedId).orElseGet { Hit(feedId, 0) }
         hit.increaseHitCount()
         hitRepository.save(hit)
-        val isMine = Objects.equals(feed.user.id, userUtil.fetchCurrentUser().id)
-        val response = DetailFeedResponseDto(feed, feedImages.map { it.url }, hit.hitCount, isMine)
+        val user = userUtil.fetchCurrentUser()
+        val likeCount = heartRepository.countByFeed(feed)
+        val isLiked = heartRepository.existsByUser(user)
+        val tagList = tagRepository.findAllByFeed(feed).map { it.title }
+        val isMine = Objects.equals(feed.user.id, user.id)
+
+        val response = DetailFeedResponseDto(
+            feed,
+            feedImages.map { it.url },
+            hit.hitCount,
+            likeCount,
+            isLiked,
+            tagList,
+            isMine
+        )
         return ResponseEntity(response, HttpStatus.OK)
     }
 }
