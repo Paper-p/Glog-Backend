ab3e7a12d9d7389176c5dc7ad0f0ba7678d31070
baegteun <baegteun@gmail.com>
2022-08-24 11:49:49 +0900

    response type

--- src/main/kotlin/baegteun/post/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/baegteun/post/domain/feed/presentation/FeedController.kt
@@ -2,8 +2,8 @@ package baegteun.post.domain.feed.presentation
 
 import baegteun.post.domain.feed.presentation.dto.request.CreateFeedRequestDto
 import baegteun.post.domain.feed.presentation.dto.request.UpdateFeedRequestDto
-import baegteun.post.domain.feed.presentation.dto.response.AllFeedListResponseDto
 import baegteun.post.domain.feed.presentation.dto.response.DetailFeedResponseDto
+import baegteun.post.domain.feed.presentation.dto.response.FeedListResponseDto
 import baegteun.post.domain.feed.services.*
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
@@ -22,15 +23,18 @@ import javax.validation.constraints.NotBlank
 @RestController
 @RequestMapping("feed")
 class FeedController(
-    private val allFeedListService: AllFeedListService,
+    private val feedListService: FeedListService,
     private val detailFeedService: DetailFeedService,
     private val createFeedService: CreateFeedService,
     private val updateFeedService: UpdateFeedService,
     private val deleteFeedService: DeleteFeedService
 ) {
     @GetMapping("list")
-    fun allFeedList(@PageableDefault(size = 5) pageable: Pageable): ResponseEntity<AllFeedListResponseDto> =
-        allFeedListService.execute(pageable)
+    fun feedList(
+        @PageableDefault(size = 5, page = 0) pageable: Pageable,
+        @RequestParam("keyword") keyword: String?
+    ): ResponseEntity<FeedListResponseDto> =
+        feedListService.execute(pageable, keyword)
 
     @GetMapping("{id}")
     fun detailFeed(@NotBlank @PathVariable("id") id: Long): ResponseEntity<DetailFeedResponseDto> =
