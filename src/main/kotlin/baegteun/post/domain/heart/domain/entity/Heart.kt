a8c8f1fb6f1a1ef5ccb46b610eb8ef3c303666a5
baegteun <baegteun@gmail.com>
2022-08-24 11:47:44 +0900

    heart entity

---
+++ src/main/kotlin/baegteun/post/domain/heart/domain/entity/Heart.kt
@@ -0,0 +1,15 @@
+package baegteun.post.domain.heart.domain.entity
+
+import baegteun.post.domain.feed.domain.entity.Feed
+import baegteun.post.domain.user.domain.entity.User
+import baegteun.post.global.entity.BaseIdEntity
+import javax.persistence.*
+
+@Entity
+class Heart(
+    @ManyToOne(fetch = FetchType.LAZY)
+    var feed: Feed,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    var user: User
+): BaseIdEntity()
